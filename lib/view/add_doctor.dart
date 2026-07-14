421c8dc629a8382cbf8b973007dff6a9b8edf2f4
sreenath262002 <sreenathg2002@gmail.com>
2026-07-14 17:20:39 +0530

    updated add doctor page

--- lib/view/add_doctor.dart
+++ lib/view/add_doctor.dart
@@ -2,22 +2,6 @@ import 'package:doctor_directory_app/services/doctor_api.dart';
 import 'package:flutter/material.dart';
 import '../models/DoctorsAll.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: AddDoctor(),
-    );
-  }
-}
-
 class AddDoctor extends StatefulWidget {
   const AddDoctor({super.key});
 
