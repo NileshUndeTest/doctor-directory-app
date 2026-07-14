35da615886ad7eef83cc9d74cf21f66c98c9b362
Abhishek P <youremail@example.com>
2026-07-14 11:33:21 +0530

    folder created

---
+++ lib/model/doctors_model.dart
@@ -0,0 +1,26 @@
+class DoctorsModel {
+  final String id;
+  final String fullName;
+  final String avatar;
+  final String qualification;
+  final String department;
+  final String age;
+  final String yearsOfExperience;
+  final String status;
+  final String about;
+  final String workExperience;
+
+
+  DoctorsModel({
+    required this.id,
+    required this.fullName,
+    required this.avatar,
+    required this.qualification,
+    required this.department,
+    required this.age,
+    required this.yearsOfExperience,
+    required this.status,
+    required this.about,
+    required this.workExperience
+  });
+
